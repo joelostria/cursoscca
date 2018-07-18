9c52d660e9d0b66e956565624cb798ba44d1b48c
Sebastian Joel Morales Ostria <jmjoelmor891@gmail.com>
2018-07-18 00:36:54 -0500

    v2

---
+++ cursos/urls.py
@@ -0,0 +1,47 @@
+"""cursos URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth.views import login,password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+
+urlpatterns = [
+    url(r'^$',index,name='inicio'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^cursos/', include('app.curso.urls',namespace='curso')),
+    url(r'^registrar/', include('app.registrar.urls',namespace='registrar')),
+    url(r'^usuario/', include('app.usuarios.urls',namespace='usuarios')),
+    url(r'^accounts/login/',login,{'template_name':'usuario/index.html'},name='login'),
+    url(r'^reset/password_reset', password_reset, 
+        {'template_name':'registration/password_reset_form.html',
+        'email_template_name': 'registration/password_reset_email.html'}, 
+        name='password_reset'), 
+    url(r'^password_reset_done', password_reset_done, 
+        {'template_name': 'registration/password_reset_done.html'}, 
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, 
+        {'template_name': 'registration/password_reset_confirm.html'},
+        name='password_reset_confirm'
+        ),
+    url(r'^reset/done', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'},
+        name='password_reset_complete'),
+   
+
+]
+
